f321dd63b6e8e22a043240406026348806adfe34
vinsmake <ps.enrique.e@gmail.com>
2023-10-23 12:54:54 -0600

    Project updated

---
+++ src/HeroApp.jsx
@@ -0,0 +1,7 @@
+export const HeroApp = () => {
+    return (
+    <>
+        <h1>React Hero App</h1>
+    </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 
 import './styles.css'
+import { HeroApp } from './HeroApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <HeroApp/>
   </React.StrictMode>,
 )
