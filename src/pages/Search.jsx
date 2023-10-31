b4e393c9168acbd8c4075be438dbcab7afbfeacc
vinsmake <ps.enrique.e@gmail.com>
2023-10-31 14:36:08 -0600

    Project updated

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,28 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { HeroCard } from "../components/HeroCard"
 import { useForm } from "../hooks/useForm"
+import queryString from "query-string";
 
 export const Search = () => {
 
     /* navigation */
     const navigate = useNavigate();
     const location = useLocation();
+
+    /* Query-String package */
+    /* this separates the query in diferent parameters. */
+    /* we can get only the q by using 'q', provided by query-string rules */
+    const {q = ''} = queryString.parse(location.search);
+
     
     /* Form */
+    /* Initial state of the form, we take this 2 data and use it in the useForm custom hook */
     const {searchText, onInputChange} = useForm({
+        /* '' as default */
         searchText: ''
     });
 
+    /* we create this function to navigate to the expected url */
     const onSearchSubmit = (e) => {
         e.preventDefault();
 
@@ -60,7 +70,7 @@ export const Search = () => {
                     </div>
 
                     <div className="alert alert-danger animate__animated animate__fadeIn" >
-                        No hero with <b></b>
+                        No hero with <b>{q}</b>
                     </div>
 
                 </div>
