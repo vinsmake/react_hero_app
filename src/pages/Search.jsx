ffde2e27ec2d6f017b460cfe86ca345a362e5ee9
vinsmake <ps.enrique.e@gmail.com>
2023-10-30 21:30:55 -0600

    Project updated

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,27 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { HeroCard } from "../components/HeroCard"
+import { useForm } from "../hooks/useForm"
 
 export const Search = () => {
+
+    /* navigation */
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    /* Form */
+    const {searchText, onInputChange} = useForm({
+        searchText: ''
+    });
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+
+        /* This cleans the text */
+        searchText.toLowerCase().trim()
+        /* this converts the text to a url request */
+        navigate(`?q=${searchText.toLowerCase().trim()}`)
+    }
+
     return (
         <>
             <h1>Search</h1>
@@ -12,13 +33,15 @@ export const Search = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form>
+                    <form onSubmit={onSearchSubmit}>
                         <input
                             type="text"
                             placeholder="Search a hero"
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value={searchText}
+                            onChange={onInputChange}
                         />
 
                         <button className="btn btn-outline-primary mt-1">
